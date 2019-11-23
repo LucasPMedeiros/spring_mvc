315440ceef4071848f383c827894f3dae00fb448
Lucas Pereira <51942384+LucasPMedeiros@users.noreply.github.com>
2019-11-22 23:33:13 -0200

    ordenando funcoes

--- src/main/java/spring_webapp/Student.java
+++ src/main/java/spring_webapp/Student.java
@@ -11,49 +11,8 @@ public class Student {
 	private String favoriteLanguage;
 	private String[] operationSystems;
 
-	
-	public String getFavoriteLanguage() {
-		return favoriteLanguage;
-	}
-
-	public void setFavoriteLanguage(String favoriteLanguage) {
-		this.favoriteLanguage = favoriteLanguage;
-	}
-
-	public void setOperationSystems(String[] operationSystems) {
-		System.out.println(operationSystems.toString());
-		this.operationSystems = operationSystems;
-	}
-	
-	public String[] getOperationSystems() {
-		System.out.println(operationSystems);
-		return operationSystems;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public Student() {
-		countryOptions = new LinkedHashMap<String, String>();
-		
-		countryOptions.put("BR", "Brazil");
-		countryOptions.put("GE", "Germany");
-		countryOptions.put("FR", "France");
-		countryOptions.put("IN", "India");
-		
-	}
-
 	public String getFirstName() {
 		return firstName;
-	}
-
-	public LinkedHashMap<String, String> getCountryOptions() {
-		return countryOptions;
 	}
 
 	public void setFirstName(String firstName) {
@@ -67,5 +26,43 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	public String getCountry() {
+		return country;
+	}
 
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public Student() {
+		countryOptions = new LinkedHashMap<String, String>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("GE", "Germany");
+		countryOptions.put("FR", "France");
+		countryOptions.put("IN", "India");
+	}
+	
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+	
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+	
+	public String[] getOperationSystems() {
+		System.out.println(operationSystems);
+		return operationSystems;
+	}
+	
+	public void setOperationSystems(String[] operationSystems) {
+		System.out.println(operationSystems.toString());
+		this.operationSystems = operationSystems;
+	}
 }
